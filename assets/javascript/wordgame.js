1a0c69104bde2b0677fa4a1e33b5d637cc442e20
Daniel Tian <danieltian31@yahoo.com>
2019-03-05 15:50:53 -0600

    added media query for tablet size and consolidated html and javascript code to one file

--- assets/javascript/wordgame.js
+++ assets/javascript/wordgame.js
@@ -1,16 +1,20 @@
-var guessWord = document.getElementById("guess-word");
-var numOfGuess = document.getElementById("guess-remaining");
-var guessedLettersText = document.getElementById("guessed-letters");
-var audio = document.querySelector("audio")
-var listOfWords = ["sith", "jedi","vader", "yoda", "tarkin", "chewbacca", "anakin", "clones"];
-var wordGuessCorrectly = false;
-var notCorrectLetter = false;
-var numGuess = 15;
-var wins = 0, losses = 0;
-var guessedLetters = [];
-var outOfGuesses, wordToGuess, blanks;
+let guessWord = document.getElementById("guess-word");
+let numOfGuess = document.getElementById("guess-remaining");
+let guessedLettersText = document.getElementById("guessed-letters");
+let audio = document.querySelector("audio");
 
-var imagesGallery = {
+let game = {listOfWords: ["sith", "jedi","vader", "yoda", "tarkin", "chewbacca", "anakin", "clones"],
+wordGuessCorrectly: false,
+notCorrectLetter: false,
+numGuess: 15,
+wins: 0,
+losses: 0,
+wordToGuess: "",
+guessedLetters: [],
+outOfGuesses: false,
+wordGuessCorrectly: "",
+blanks: "",
+imagesGallery: {
     "sith": { image: "assets/images/sith.jpg",
               sound: "assets/sounds/sith.mp3"
              },
@@ -38,38 +42,35 @@ var imagesGallery = {
     "clones": { image: "assets/images/clone.jpg",
             sound: "assets/sounds/clone.ogg"
     }
-}
+},
 
-// A random index is selected from words list and fills the space with blank
-function selectRandomWord(){
-    numOfGuess.textContent = numGuess;
-    var i = Math.floor(Math.random()*listOfWords.length);
-    return listOfWords[i];
-}
+selectRandomWord: function(arr){
+    numOfGuess.textContent = this.numGuess;
+    var i = Math.floor(Math.random()*arr.length);
+    return arr[i];
+},
 
-function fillBlank(word){
-    blanks = "";
+fillBlank: function(word) {
     for (var x = 0; x < word.length; x++){
-        blanks = blanks + "-";
-    guessWord.textContent = blanks;
-}
-}
+        this.blanks = this.blanks + "-";
+        console.log(this.blanks);
+    guessWord.textContent = this.blanks;
+    }
+},
 
-//Function resets the game after the word has been correctly guessed.
-function reset(){
-    guessedLetters = [];
-    outOfGuesses= false;
-    wordGuessCorrectly = false;
-    numGuess = 15;
+reset: function() {
+    this.guessedLetters = [];
+    this.outOfGuesses = false;
+    this.wordGuessCorrectly = false;
+    this.numGuess = 15;
+    this.blanks = "";
     guessWord.textContent= "";
     guessedLettersText.textContent= "";
-    wordToGuess = selectRandomWord()
-    fillBlank(wordToGuess);
+    this.wordToGuess = this.selectRandomWord(this.listOfWords)
+    this.fillBlank(this.wordToGuess);
+},
 
-}
-
-//Function replaces blanks with corrected input keyboard letter
-function replaceChar(str, ind, letter){
+replaceChar: function(str, ind, letter){
     if (ind === 0){
         str = letter + str.substring(1, str.length);
     }
@@ -77,25 +78,35 @@ function replaceChar(str, ind, letter){
         str = str.substring(0, ind) + letter + str.substring(ind+1);
      }
      return str;
-}
+},
 
-function playerWins(){
+playerWins: function(){
     alert("Congratulations for solving the words.");
-    document.querySelector("#char-pic").src = imagesGallery[wordToGuess]["image"];
-    wins++;
-    document.querySelector("#name").textContent = wordToGuess;
-    document.getElementById("num-of-wins").textContent = wins;
-    document.querySelector("audio").src = imagesGallery[wordToGuess]["sound"];
+    console.log(this.imagesGallery);
+    console.log(this.wordToGuess);
+    document.querySelector("#char-pic").src = this.imagesGallery[this.wordToGuess]["image"];
+    this.wins++;
+    document.querySelector("#name").textContent = this.wordToGuess;
+    document.getElementById("num-of-wins").textContent = this.wins;
+    document.querySelector("audio").src = this.imagesGallery[this.wordToGuess]["sound"];
     document.querySelector("audio").play();
+    game.reset();
+},
+
+playerLoses: function() {
+    alert("Sorry you lost.");
+    this.losses++;
+    document.getElementById("num-of-losses").textContent = this.losses;
     reset();
 }
 
-function playerLoses(){
-    alert("Sorry you lost.");
-    losses++;
-    document.getElementById("num-of-losses").textContent = losses;
-    reset();
+
+
+
+
 }
+
+
 
 //Tracks the user input and runs the game.
 function trackGame(event){
@@ -108,67 +119,166 @@ function trackGame(event){
 
     else{
         ///For loop checks if the input letter matches correctly to the word that needs to be guessed and adds the index of that letter to array listLetterInd.
-        var listLetterInd = [];
-        for (var i = 0; i < wordToGuess.length;i++){
-            if (wordToGuess[i] === letter.toLowerCase()){
-                listLetterInd.push(i);
+        var listLetterIndArr = [];
+        for (var i = 0; i < game.wordToGuess.length;i++){
+            if (game.wordToGuess[i] === letter.toLowerCase()){
+                listLetterIndArr.push(i);
             }
         }
 
 
-        var listLetterIndIsNotEmpty  = (listLetterInd.length > 0);
+            var listLetterIndIsNotEmpty  = (listLetterIndArr.length > 0);
             if (listLetterIndIsNotEmpty){
                 while (listLetterIndIsNotEmpty){
-                    var p = listLetterInd.pop();
-                    blanks = replaceChar(blanks, p, letter);
-                    listLetterIndIsNotEmpty = !(listLetterInd.length === 0);
+                    var p = listLetterIndArr.pop();
+                    game.blanks = game.replaceChar(game.blanks, p, letter);
+                    listLetterIndIsNotEmpty = !(listLetterIndArr.length === 0);
                 }
-                guessWord.textContent = blanks;
+                guessWord.textContent = game.blanks;
 
-                wordGuessCorrectly = (guessWord.textContent === wordToGuess);
+                game.wordGuessCorrectly = (guessWord.textContent === game.wordToGuess);
             }
             else{
-                notCorrectLetter = true;
+                game.notCorrectLetter = true;
             }
 
             //Updates number of guesses remaining and guessed letter array by adding user input
-            if((guessedLetters.length === 0) || (guessedLetters.indexOf(letter.toLowerCase()) === -1)){
-                numGuess--;
-                numOfGuess.textContent = numGuess;
-                guessedLetters.push(letter.toLowerCase());
-                if(blanks.indexOf(letter.toLowerCase()) === -1){
+            if((game.guessedLetters.length === 0) || (game.guessedLetters.indexOf(letter.toLowerCase()) === -1)){
+                game.numGuess--;
+                numOfGuess.textContent = game.numGuess;
+                game.guessedLetters.push(letter.toLowerCase());
+                if(game.blanks.indexOf(letter.toLowerCase()) === -1){
                     guessedLettersText.textContent += (" " + letter);
                  }
              
             }
         }
 
-    outOfGuesses = (numGuess === 0);
+    game.outOfGuesses = (game.numGuess === 0);
 
-    if(outOfGuesses){
-        if(wordGuessCorrectly){
-            playerWins();
+    if(game.outOfGuesses){
+        if(game.wordGuessCorrectly){
+            game.playerWins();
         }
         else{
-            playerLoses();
+            game.playerLoses();
         }
     }
-    else if(wordGuessCorrectly){
-        playerWins();
+    else if(game.wordGuessCorrectly){
+        game.playerWins();
     }
     
 }
     
 
 //Starting game function
-function init(){
-    wordToGuess = selectRandomWord();
-    fillBlank(wordToGuess);
+function main(){
+    game.wordToGuess = game.selectRandomWord(game.listOfWords);
+    console.log('xxx');
+    game.fillBlank(game.wordToGuess);
 
     document.addEventListener("keydown", trackGame);
 }
 
-t1 = performance.now();
-document.onload = init();
-t2 = performance.now();
-console.log(t1+t2/1000);
+document.onload = main();
+
+
+
+
+// var guessWord = document.getElementById("guess-word");
+// var numOfGuess = document.getElementById("guess-remaining");
+// var guessedLettersText = document.getElementById("guessed-letters");
+// var audio = document.querySelector("audio");
+// var listOfWords = ["sith", "jedi","vader", "yoda", "tarkin", "chewbacca", "anakin", "clones"];
+// var wordGuessCorrectly = false;
+// var notCorrectLetter = false;
+// var numGuess = 15;
+// var wins = 0, losses = 0;
+// var guessedLetters = [];
+// var outOfGuesses, wordToGuess, blanks;
+// var imagesGallery = {
+//     "sith": { image: "assets/images/sith.jpg",
+//               sound: "assets/sounds/sith.mp3"
+//              },
+//     "jedi": { image: "assets/images/jedi.jpg",
+//              sound: "assets/sounds/jedi.mp3"
+//             }, 
+//     "vader": { image: "assets/images/vader.jpg",
+//             sound: "assets/sounds/vader.mp3"
+//             },
+//     "yoda": { image: "assets/images/yoda.jpg",
+//             sound: "assets/sounds/yoda.mp3"
+//              },
+//     "tarkin": { image: "assets/images/tarkin.jpg",
+//              sound: "assets/sounds/tarkin.mp3"
+//             },
+
+//     "chewbacca": { image:"assets/images/chewbacca.jpg",
+//             sound: "assets/sounds/chewbacca.wav"
+//             },
+
+//     "anakin": { image: "assets/images/anakin.jpg",
+//             sound:"assets/sounds/anakin.mp3"
+
+//     },
+//     "clones": { image: "assets/images/clone.jpg",
+//             sound: "assets/sounds/clone.ogg"
+//     }
+// }
+
+// // A random index is selected from words list and fills the space with blank
+// function selectRandomWord(){
+//     numOfGuess.textContent = numGuess;
+//     var i = Math.floor(Math.random()*listOfWords.length);
+//     return listOfWords[i];
+// }
+
+// function fillBlank(word){
+//     blanks = "";
+//     for (var x = 0; x < word.length; x++){
+//         blanks = blanks + "-";
+//     guessWord.textContent = blanks;
+// }
+// }
+
+// //Function resets the game after the word has been correctly guessed.
+// function reset(){
+//     guessedLetters = [];
+//     outOfGuesses= false;
+//     wordGuessCorrectly = false;
+//     numGuess = 15;
+//     guessWord.textContent= "";
+//     guessedLettersText.textContent= "";
+//     wordToGuess = selectRandomWord()
+//     fillBlank(wordToGuess);
+
+// }
+
+// //Function replaces blanks with corrected input keyboard letter
+// function replaceChar(str, ind, letter){
+//     if (ind === 0){
+//         str = letter + str.substring(1, str.length);
+//     }
+//     else{
+//         str = str.substring(0, ind) + letter + str.substring(ind+1);
+//      }
+//      return str;
+// }
+
+// function playerWins(){
+//     alert("Congratulations for solving the words.");
+//     document.querySelector("#char-pic").src = imagesGallery[wordToGuess]["image"];
+//     wins++;
+//     document.querySelector("#name").textContent = wordToGuess;
+//     document.getElementById("num-of-wins").textContent = wins;
+//     document.querySelector("audio").src = imagesGallery[wordToGuess]["sound"];
+//     document.querySelector("audio").play();
+//     reset();
+// }
+
+// function playerLoses(){
+//     alert("Sorry you lost.");
+//     losses++;
+//     document.getElementById("num-of-losses").textContent = losses;
+//     reset();
+// }
